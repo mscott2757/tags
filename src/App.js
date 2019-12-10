b1aba5cdfe6897204cc87e89a05000f505db2927
mscott2757 <mscott2757@berkeley.edu>
2019-12-10 01:25:58 -0800

    update sass

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './styles/css/App.css';
+import './styles/App.scss';
 import { TagSearch, Tags, TagsOutput, Notification } from './components';
 
 const App = () => (
