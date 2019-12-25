9fa0ec1f810f348a7e81769c0d3bca5902a82563
mscott2757 <mscott2757@berkeley.edu>
2019-12-25 12:53:32 -0800

    disable auto focus

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -1,3 +1,4 @@
+import { compose, withProps } from 'recompose';
 import { WithContext as ReactTags } from 'react-tag-input';
 import { connect } from 'react-redux';
 import {
@@ -6,17 +7,20 @@ import {
   reorderTag
 } from '../actions';
 
-export const Tags = connect(
-  ({ tags }) => ({ tags }),
-  dispatch => ({
-    handleDelete: i => {
-      dispatch(deleteTag(i));
-    },
-    handleAddition: text => {
-      dispatch(addTag({ text }));
-    },
-    handleDrag: (tag, currPos, newPos) => {
-      dispatch(reorderTag(tag, currPos, newPos));
-    }
-  }),
+export const Tags = compose(
+  withProps(() => ({ autofocus: false })),
+  connect(
+    ({ tags }) => ({ tags }),
+    dispatch => ({
+      handleDelete: i => {
+        dispatch(deleteTag(i));
+      },
+      handleAddition: text => {
+        dispatch(addTag({ text }));
+      },
+      handleDrag: (tag, currPos, newPos) => {
+        dispatch(reorderTag(tag, currPos, newPos));
+      }
+    }),
+  ),
 )(ReactTags);
